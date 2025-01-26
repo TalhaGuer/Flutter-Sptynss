92a3dbccedcdcee6cd4aa6f91c1f5cff20e1c1e7
Talha Güleryüz <162495684+TalhaGuer@users.noreply.github.com>
2025-01-27 00:23:52 +0300

    Main Dart Güncelledim
    main dart dosyamı diğer sayfalarıma göre ayarlayıp düzenledim

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'core/themes.dart';
+import 'screens/home_screen.dart';
+import 'screens/download_music_screen.dart';
+import 'screens/loading_screen.dart';
 import 'routes/app_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,12 +20,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Spotify Clone',
-      theme: ThemeData.dark(),
-      onGenerateRoute: AppRouter.generateRoute,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: themeProvider.isDark ? ThemeMode.dark : ThemeMode.light,
       initialRoute: '/loading',
+      onGenerateRoute: AppRouter.generateRoute,
     );
   }
 }
