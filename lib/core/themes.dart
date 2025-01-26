2f3f608de6fb158c56ad4aafcd43f2a2d5623cbc
Talha Güleryüz <162495684+TalhaGuer@users.noreply.github.com>
2025-01-27 00:33:46 +0300

    Themes dart dosyası eklendi
    Provider eklenerek gece gündüz modu sağlandı kod içerisindeki renkler ve temalar için bilgiler girildi

---
+++ lib/core/themes.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider with ChangeNotifier {
+  bool _isDark = false;
+  bool get isDark => _isDark;
+
+  void toggleTheme() {
+    _isDark = !_isDark;
+    notifyListeners();
+  }
+}
+
+const lightColors = {
+  "primary": Color.fromARGB(255, 65, 159, 217),
+  "onPrimary": Color.fromARGB(255, 255, 255, 255),
+  "secondary": Color.fromARGB(255, 241, 241, 241),
+  "onSecondary": Color.fromARGB(255, 82, 76, 100),
+  "surface": Color.fromARGB(255, 255, 255, 255),
+  "onSurface": Color.fromARGB(255, 82, 76, 100),
+  "success": Colors.green,
+  "error": Colors.red,
+  "onError": Colors.white,
+};
+
+const darkColors = {
+  "primary": Color.fromARGB(255, 43, 82, 120),
+  "onPrimary": Color.fromARGB(255, 241, 241, 241),
+  "secondary": Color.fromARGB(255, 32, 43, 54),
+  "onSecondary": Color.fromARGB(255, 241, 241, 241),
+  "surface": Color.fromARGB(255, 23, 33, 43),
+  "onSurface": Color.fromARGB(255, 245, 245, 245),
+  "success": Colors.green,
+  "error": Colors.red,
+  "onError": Colors.white,
+};
+
+final lightTheme = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: ColorScheme(
+    brightness: Brightness.light,
+    primary: lightColors["primary"]!,
+    onPrimary: lightColors["onPrimary"]!,
+    secondary: lightColors["secondary"]!,
+    onSecondary: lightColors["onSecondary"]!,
+    error: lightColors["error"]!,
+    onError: lightColors["onError"]!,
+    surface: lightColors["surface"]!,
+    onSurface: lightColors["onSurface"]!,
+  ),
+  textTheme: TextTheme(
+    bodySmall: TextStyle(fontSize: 12, color: lightColors["onSurface"]),
+    bodyMedium: TextStyle(fontSize: 14, color: lightColors["onSurface"]),
+    bodyLarge: TextStyle(fontSize: 16, color: lightColors["onSurface"]),
+  ),
+);
+
+final darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(
+    primary: darkColors["primary"]!,
+    onPrimary: darkColors["onPrimary"]!,
+    secondary: darkColors["secondary"]!,
+    onSecondary: darkColors["onSecondary"]!,
+    error: darkColors["error"]!,
+    onError: darkColors["onError"]!,
+    surface: darkColors["surface"]!,
+    onSurface: darkColors["onSurface"]!,
+  ),
+  textTheme: TextTheme(
+    bodySmall: TextStyle(fontSize: 12, color: darkColors["onSurface"]),
+    bodyMedium: TextStyle(fontSize: 14, color: darkColors["onSurface"]),
+    bodyLarge: TextStyle(fontSize: 16, color: darkColors["onSurface"]),
+  ),
+);
